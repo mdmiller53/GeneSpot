ff82751338ceb10ca18a8b16a2441469e9bab261
Hector Rovira <hrovira@systemsbiology.org>
2013-11-26 16:58:49 -0800

    integrated views

--- app/scripts/views/gs/atlas_quick_tutorial.js
+++ app/scripts/views/gs/atlas_quick_tutorial.js
@@ -1,7 +1,7 @@
 define(["jquery", "underscore", "backbone", "hbs!templates/gs/atlas_quick_tutorial"],
     function ($, _, Backbone, Tpl) {
         return Backbone.View.extend({
-            initialize: function () {
+            initialize: function (options) {
                 var _id = Math.round(Math.random() * 100000);
                 this.$el.html(Tpl({"id": _id}));
             }

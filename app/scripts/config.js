189df0d5bf986b8a99f8d544ea9e733b9ea17961
Hector Rovira <hrovira@systemsbiology.org>
2014-01-30 14:10:19 -0800

    updated versions of carve and visquick

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,29 +1,29 @@
 require.config({
-    baseUrl: 'scripts',
+    baseUrl: "scripts",
 
     paths: {
-        jquery: '../bower_components/jquery/jquery',
+        "jquery": "../bower_components/jquery/jquery",
         "jQuery-ui": "../bower_components/jquery-ui/ui/jquery-ui",
         "jquery-event-drag" : "../bower_components/jquery-event-drag/index",
         "jquery-event-drop" : "../bower_components/jquery-event-drop/index",
-        backbone: '../bower_components/backbone/backbone',
-        bootstrap: '../bower_components/bootstrap/js/bootstrap',
-        d3: '../bower_components/d3/d3',
-        modernizr: '../bower_components/modernizr',
-        underscore: '../bower_components/underscore/underscore',
-        hbs : '../bower_components/require-handlebars-plugin/hbs',
-        handlebars : '../bower_components/require-handlebars-plugin/Handlebars',
-        'json2' : '../bower_components/require-handlebars-plugin/hbs/json2',
-        'i18nprecompile' : '../bower_components/require-handlebars-plugin/hbs/i18nprecompile',
-        vq : '../bower_components/visquick/vq',
-        "jsPlumb" : '../bower_components/jsPlumb/dist/js/jquery.jsPlumb-1.5.4-min',
+        "backbone": "../bower_components/backbone/backbone",
+        "bootstrap": "../bower_components/bootstrap/js/bootstrap",
+        "d3": "../bower_components/d3/d3",
+        "modernizr": "../bower_components/modernizr",
+        "underscore": "../bower_components/underscore/underscore",
+        "hbs": "../bower_components/require-handlebars-plugin/hbs",
+        "handlebars" : "../bower_components/require-handlebars-plugin/Handlebars",
+        "json2" : "../bower_components/require-handlebars-plugin/hbs/json2",
+        "i18nprecompile" : "../bower_components/require-handlebars-plugin/hbs/i18nprecompile",
+        "vq" : "../bower_components/visquick/vq",
+        "jsPlumb" : "../bower_components/jsPlumb/dist/js/jquery.jsPlumb-1.5.4-min",
         "colorbrewer": "../bower_components/colorbrewer/colorbrewer",
         "stacksvis" : "../bower_components/stacksvis/js/stacksvis",
         "carve" : "../bower_components/carve/carve"
     },
-    shim: {
-        underscore : {
-            'exports' : '_'
+    "shim": {
+        "underscore" : {
+            "exports" : "_"
         },
         "jQuery-ui" : {
             "deps": ["jquery"],
@@ -37,33 +37,26 @@ require.config({
             "deps": ["jquery"],
             "exports" : "$"
         },
-        d3 : {
-            'exports' : 'd3'
-        },
-        vq : {
-            'deps' : ['d3','underscore'],
-            'exports' : 'vq'
-        },
-        backbone : {
-            deps: ['underscore', 'jquery'],
-            exports: 'Backbone'
+        "backbone" : {
+            "deps": ["underscore", "jquery"],
+            exports: "Backbone"
         },
         bootstrap : {
-            deps : ['jquery','jQuery-ui'],
-            exports : 'bootstrap'
+            deps : ["jquery","jQuery-ui"],
+            exports : "bootstrap"
         }
     },
     hbs : {
-        "templateExtension" : 'hbs',
+        "templateExtension" : "hbs",
         "disableI18n" : true,
-        "helperPathCallback" : // Callback to determine the path to look for helpers
-            function (name) { // ('/template/helpers/'+name by default)
-                return 'templates/helpers/' + name;
+        "helperPathCallback" :
+            function (name) {
+                return "templates/helpers/" + name;
             }
     }
 });
 
-require(['webapp'], function (WebApp) {
+require(["webapp"], function (WebApp) {
     WebApp.initialize();
 });
 

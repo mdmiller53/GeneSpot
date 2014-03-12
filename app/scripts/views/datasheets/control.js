c1415ecd6bfdb342cbee961579488eb58ca9684a
Hector Rovira <hrovira@systemsbiology.org>
2014-03-12 16:08:52 -0700

    refactored sheets creation and listing

--- app/scripts/views/datasheets/control.js
+++ app/scripts/views/datasheets/control.js
@@ -1,12 +1,9 @@
 define(["jquery", "underscore", "backbone", "base64",
-    "views/datasheets/itemizer", "hbs!templates/datasheets/container",
-    "hbs!templates/datasheets/needs_login", "hbs!templates/datasheets/drive_folder_link"],
-    function ($, _, Backbone, base64, Itemizer, Tpl, NeedsLoginTpl, DriveFolderLinkTpl) {
+    "hbs!templates/datasheets/container", "hbs!templates/datasheets/needs_login"],
+    function ($, _, Backbone, base64, Tpl, NeedsLoginTpl) {
         return Backbone.View.extend({
-            itemizers: {},
-            datasheets: new Backbone.Collection([], { "url": "svc/collections/datasheets" }),
             folder: new Backbone.Model(),
-            application_data: new Backbone.Model({}, { "url": "svc/auth/providers/google_drive/drive/v2/files" } ),
+            files: new Backbone.Model({}, { "url": "svc/auth/providers/google_drive/drive/v2/files" } ),
 
             events: {
                 "click .create-datasheets": function() {
@@ -20,29 +17,23 @@ define(["jquery", "underscore", "backbone", "base64",
                         return;
                     }
 
-                    var labels = _.map(this.datasheets["models"], function(gl_model) {
-                        return gl_model.get("label");
-                    });
+                    var labels = _.pluck(this.files.get("items"), "title");
                     if (labels.indexOf(newname) >= 0) {
                         WebApp.alert(this.$(".duplicate-datasheets-name"), 3000);
                         return;
                     }
 
-                    // TODO: Create sheet in Google Drive
-                    Backbone.sync("create", new Backbone.Model({ "label": newname }), {
-                        "url": "svc/collections/datasheets", "success": this.render
-                    });
-
-                    WebApp.alert(this.$(".datasheets-added-success"));
-                },
-
-                "click .remove-datasheets": function(e) {
-                    console.debug("views/datasheets/control.click:remove-datasheets");
-
-                    var listid = $(e.target).data("id");
-                    Backbone.sync("delete", new Backbone.Model({}), {
-                        "url": "svc/collections/datasheets/" + listid, "success": this.render
-                    });
+                    new Backbone.Model().save(
+                        {
+                            "title": newname,
+                            "parents": [ {"id": this.folder.get("id")} ],
+                            "mimeType": "application/vnd.google-apps.spreadsheet"
+                        },
+                        {
+                            "url": "svc/auth/providers/google_drive/drive/v2/files",
+                            "method": "POST",
+                            "success": this.render
+                        });
                 },
 
                 "click .refresh-datasheets": function() {
@@ -51,72 +42,34 @@ define(["jquery", "underscore", "backbone", "base64",
             },
 
             initialize: function() {
-                _.bindAll(this, "render", "__load", "__render");
-                _.bindAll(this, "__load_application_data", "__load_folder", "__create_folder");
-
-                this.datasheets.on("change", function(item) {
-                    console.debug("views/datasheets/control.datasheets:change");
-                    if (_.isEmpty(item)) return;
-                    Backbone.sync("update", item, {
-                        "url": "svc/collections/datasheets/" + item.get("id"), "success": this.render
-                    });
-                });
+                _.bindAll(this, "render", "__load", "__render", "__create_folder");
             },
 
             render: function() {
                 console.debug("views/datasheets/control.render");
 
-                this.datasheets.fetch({ "success": this.__load });
-                this.application_data.fetch({ "success": this.__load_application_data });
+                this.files.fetch({ "success": this.__load });
 
                 this.$el.html(NeedsLoginTpl());
                 return this;
             },
 
-            __load: function() {
-                var datasheets = _.map(this.datasheets["models"], function(gl_model) {
-                    return { "id": gl_model.get("id"), "label": gl_model.get("label") };
-                });
-
-                this.$el.html(Tpl({ "datasheets": _.sortBy(datasheets, "sort") }));
-                _.each(this.datasheets["models"], this.__render, this);
-            },
-
-            __render: function(i_model) {
-                var $datasheet = this.$("#tab-datasheets-" + i_model.get("id")).find(".datasheets-infos");
-                var itemizer = this.itemizers[i_model.get("id")] = new Itemizer({"el": $datasheet, "model": i_model });
-                itemizer.render();
-            },
-
-            add_to_current: function(new_map) {
-                console.debug("views/datasheets/control.add_to_current:" + new_map.id);
-                var currentId = this.$(".nav-tabs").find("li.active").data("id");
-                var currentSheet = _.findWhere(this.datasheets["models"], { "id": currentId });
-                if (currentSheet) {
-                    var existing_sheets = _.map(currentSheet.get("maps"), function(m) { return m; });
-                    existing_sheets.push(new_map);
-                    currentSheet.set("maps", existing_sheets);
-                }
-            },
-
-            __load_application_data: function(model) {
-                console.debug("views/datasheets/control.__load_application_data");
+            __load: function(model) {
+                console.debug("views/datasheets/control.__load");
                 var folderInfo = _.findWhere(model.get("items"), { "title": "GeneSpot | Data Sheets" });
                 if (folderInfo) {
                     this.folder = new Backbone.Model(folderInfo);
-                    _.defer(this.__load_folder);
+                    _.defer(this.__render);
                 } else {
                     _.defer(this.__create_folder);
                 }
             },
 
-            __load_folder: function() {
-                console.debug("views/datasheets/control.__load_folder");
-                this.$(".drive-folder-link").html(DriveFolderLinkTpl({
-                    "title": this.folder.get("title"),
-                    "alternateLink": this.folder.get("alternateLink"),
-                    "iconLink": this.folder.get("iconLink")
-                }));
+            __render: function() {
+                console.debug("views/datasheets/control.__render");
+                var onlysheets = { "mimeType": "application/vnd.google-apps.spreadsheet" };
+                var datasheets = _.where(this.files.get("items"), onlysheets);
+                this.$el.html(Tpl({ "datasheets": _.sortBy(datasheets, "title"), "folder": this.folder.toJSON() }));
             },
 
             __create_folder: function() {

b6312aac35c3b20e41692cc0ea079ea18325f993
Hector Rovira <hrovira@systemsbiology.org>
2014-04-24 16:40:35 -0700

    generalize clinical variables to clinical and sample variables

--- db/scripts/generate_clinical_variables_lookup.js
+++ db/scripts/generate_clinical_variables_lookup.js
@@ -3,14 +3,21 @@
 
 Usage:
 
-    mongo --host=$HOST $DB_NAME generate_clinical_variables_lookup.js
+    mongo --host=$HOST $DB_NAME generate_clinical_variables_lookup.js --eval="var lookupsDbUri='hostname:port/LOOKUPS';"
  */
+
+var lookupsDb = connect(lookupsDbUri);
 
 var appendGlobalCV = function(doc) {
     // upsert to avoid duplicates!
-    db.getSiblingDB("LOOKUPS").clinical_variables.update({ "id": doc["id"] }, { "id": doc["id"], "label": doc["label"] }, true);
+    lookupsDb.clinical_variables.update({ "id": doc["id"] }, { "id": doc["id"], "label": doc["label"] }, true);
 }
 
+print("initial check=LOOKUPS:" + lookupsDb.clinical_variables.count());
+
 db.feature_matrix.find({"source":"CLIN"},{"label": true, "id": true}).forEach(appendGlobalCV);
+db.feature_matrix.find({"source":"SAMP"},{"label": true, "id": true}).forEach(appendGlobalCV);
+
+print("final check=LOOKUPS:" + lookupsDb.clinical_variables.count());
 
 print("completed");
--- db/scripts/generate_sample_types_lookup.js
+++ db/scripts/generate_sample_types_lookup.js
@@ -3,22 +3,24 @@
 
 Usage:
 
-    mongo --host=$HOST generate_clinical_variables_lookup.js --eval="var tumor_type='BLCA'"
+    mongo --host=$HOST $DB_NAME generate_sample_types_lookup.js --eval="var lookupsDbUri='hostname:port/LOOKUPS';"
  */
 
 var query = { "id": "C:SAMP:TNtype:::::" };
 var projection = { "_id": false };
+var tumor_type = db["name"];
 
-var sourceDb = db.getSiblingDB(tumor_type);
-var lookupsDb = db.getSiblingDB("LOOKUPS");
+var lookupsDb = connect(lookupsDbUri);
 
-print("initial check=" + tumor_type + ":" + sourceDb.feature_matrix.find(query, projection).count());
+print("initial check=" + tumor_type + ":" + db.feature_matrix.find(query, projection).count());
+print("initial check=LOOKUPS:" + lookupsDb.sample_types.count());
 
 var eachFn = function(doc) {
     doc["tumor_type"] = tumor_type;
     lookupsDb.sample_types.insert(doc);
 };
 
-sourceDb.feature_matrix.find(query, projection).forEach(eachFn);
+db.feature_matrix.find(query, projection).forEach(eachFn);
 
+print("final check=LOOKUPS:" + lookupsDb.sample_types.count());
 print("completed");
